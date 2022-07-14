a07c7353d1f0216aa596dce428ce5672da7e3c17
lbferrand <lbferrand@gmail.com>
2022-07-14 00:55:32 -0400

    Create Home page product component HeroBanner

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,17 @@ import React from "react";
 import { client } from "../lib/client";
 import { FooterBanner, HeroBanner, Product } from "../components/index";
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
       <div className="products-heading">
         <h2>Best Seller Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
       <FooterBanner />
     </>
@@ -25,6 +26,10 @@ export const getServerSideProps = async () => {
 
   const bannerQuery = '*[_type == "banner"]';
   const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
