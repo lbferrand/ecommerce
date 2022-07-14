5cc3c8d17d5260a4d3f49883cfce69a04891f478
lbferrand <lbferrand@gmail.com>
2022-07-13 22:33:20 -0400

    Add components

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,30 @@
 import React from "react";
 
+import { client } from "../lib/client";
+import { FooterBanner, HeroBanner, Product } from "../components/index";
+
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
       <div className="products-heading">
-        <h2>Best Selling Products</h2>
+        <h2>Best Seller Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
         {["Product 1", "Product 2"].map((product) => product)}
       </div>
-      Footer
+      <FooterBanner />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
 };
 
 export default Home;
